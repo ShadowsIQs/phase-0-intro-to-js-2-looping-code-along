8fc4cb6f4d5c92c1917bad98bf17fc5719700c6d
Ryan <griffinryan77@gmail.com>
2023-12-18 21:10:54 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+const messages = []
+
+
+function writeCards(kids, eventName){
+    for (let i = 0; i < kids.length;  i++){
+    messages.push (`Thank you, ${kids[i]}, for the wonderful ${eventName} gift!`)
+    }
+    return messages
+}
+
+function countDown(){
+    let x = 10;
+   while (x >= 0){
+    console.log(x);
+    x--;
+   }
+   
+}
+
+console.log(countDown)
